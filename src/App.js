a2845a867b8faa7880435b860b9895a4aebe16c1
Nelly <$>
2023-01-03 20:34:12 -0500

    installed react icons

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from "react";
 
+const style = {
+  bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#2F80ED] to-[#1CB5E0]`
+}
 function App() {
   return (
-    <div className="App">
+    <div className={style.bg}>
+      <div className={style.container}>
+      <h3 className={style.heading}>Todo App</h3>
+      <form className={style.form}>
+          <input className={style.input} type="text" placeholder="Add Todo" />
+      </form>
+      </div>
       
     </div>
   );
