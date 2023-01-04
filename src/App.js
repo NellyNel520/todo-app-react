890d86e6433ce889169baa96f8f570cb01f9227d
Nelly <$>
2023-01-03 20:42:32 -0500

    imported react icon and added the AI plus symbol

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {AiOutlinePlus} from 'react-icons/ai';
 
 const style = {
   bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#2F80ED] to-[#1CB5E0]`
@@ -10,6 +11,7 @@ function App() {
       <h3 className={style.heading}>Todo App</h3>
       <form className={style.form}>
           <input className={style.input} type="text" placeholder="Add Todo" />
+          <button className={style.button}><AiOutlinePlus size={30} /></button>
       </form>
       </div>
       
