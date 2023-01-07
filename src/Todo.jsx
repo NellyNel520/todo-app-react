5dbdaa496acf8a372ef11320b394f49cc92f1eea
Nelly <$>
2023-01-06 23:17:10 -0500

    added trashcan icon

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
-
+import {FaRegTrashAlt} from 'react-icons/fa'
 const style = {
 
 
 }
 
-const Todo = () => {
+const Todo = ({todo}) => {
   return (
     <li className={style.li}>
       <div className={style.row}>
         <input type="checkbox" />
         <p className={style.text}>{todo}</p>
       </div>
+      <button>{<FaRegTrashAlt />}</button>
 
     </li>
   )
